6e0032be21ac84e4e3b63c29da8cb570295dd31c
Duyet Le <5009534+duyet@users.noreply.github.com>
2021-07-24 22:50:09 +0700

    Update property.ts

--- src/property.ts
+++ src/property.ts
@@ -62,7 +62,7 @@ class Property extends BaseProperty {
     }
 
     name() {
-      return this.mongoosePath.path
+      return this.mongoosePath.alias || this.mongoosePath.path
     }
 
     isEditable() {
